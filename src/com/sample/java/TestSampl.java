70c93d941395874dfe8438b825de484f47904ec6
yogymax <yogymax@gmail.com>
2021-03-31 22:22:04 +0530

    Update TestSampl.java

--- src/com/sample/java/TestSampl.java
+++ src/com/sample/java/TestSampl.java
@@ -4,5 +4,7 @@ public class TestSampl {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("New Line Added1..");
+		System.out.println("New Line Added2..");
+		
 	}
 }
